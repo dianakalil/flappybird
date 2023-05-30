13a01c7679a1b75250561975fbcd01d085a0567c
dianakalil <85447680+dianakalil@users.noreply.github.com>
2023-05-30 23:18:19 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1685459797,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"media/jump10.m4a",
+		"media/jump10.ogg",
+		"media/flappy bird theme song.m4a",
+		"media/flappy bird theme song.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
